970757e6e5dd3e90c501d057523e30d3cbb5dc78
ShreyashSoni <yashsoni96@gmail.com>
2020-01-04 17:08:35 +0530

    Making Predictions with the Model

--- packages/regression_model/regression_model/pipeline.py
+++ packages/regression_model/regression_model/pipeline.py
@@ -2,7 +2,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.linear_model import Lasso
 from sklearn.preprocessing import MinMaxScaler
 
-import preprocessors as pp
+from regression_model import preprocessors as pp
 
 
 # categorical variables with NA in train set
@@ -29,29 +29,28 @@ CATEGORICAL_VARS = ['MSZoning', 'Neighborhood', 'RoofStyle', 'MasVnrType',
                     'KitchenQual', 'FireplaceQu', 'GarageType', 'GarageFinish',
                     'PavedDrive']
 
-PIPELINE_NAME = 'lasso_regression'
 
 price_pipe = Pipeline(
     [
         ('categorical_imputer',
-         pp.CategoricalImputer(variables=CATEGORICAL_VARS_WITH_NA)),
-        ('numerical_imputer', 
-         pp.NumericalImputer(variables=NUMERICAL_VARS_WITH_NA)),
-        ('temporal_variable', 
-         pp.TemporalVariableEstimator(
-             variables=TEMPORAL_VARS,
-             reference_variable=TEMPORAL_VARS)),
-        ('rare_label_encoder', 
-         pp.RareLabelCategoricalEncoder(
-             tol=0.01,
-             variables=CATEGORICAL_VARS)),
-        ('categorical_encoder', 
-         pp.CategoricalEncoder(variables=CATEGORICAL_VARS)),
-        ('log_transformer', 
-         pp.LogTransformer(variables=NUMERICALS_LOG_VARS)), 
-        ('drop_features', 
-         pp.DropUnnecessaryFeatures(variables_to_drop=DROP_FEATURES)),
-        ('scaler', MinMaxScaler()), 
+            pp.CategoricalImputer(variables=CATEGORICAL_VARS_WITH_NA)),
+        ('numerical_inputer',
+            pp.NumericalImputer(variables=NUMERICAL_VARS_WITH_NA)),
+        ('temporal_variable',
+            pp.TemporalVariableEstimator(
+                variables=TEMPORAL_VARS,
+                reference_variable=DROP_FEATURES)),
+        ('rare_label_encoder',
+            pp.RareLabelCategoricalEncoder(
+                tol=0.01,
+                variables=CATEGORICAL_VARS)),
+        ('categorical_encoder',
+            pp.CategoricalEncoder(variables=CATEGORICAL_VARS)),
+        ('log_transformer',
+            pp.LogTransformer(variables=NUMERICALS_LOG_VARS)),
+        ('drop_features',
+            pp.DropUnnecessaryFeatures(variables_to_drop=DROP_FEATURES)),
+        ('scaler', MinMaxScaler()),
         ('Linear_model', Lasso(alpha=0.005, random_state=0))
     ]
 )
